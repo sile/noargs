6faa4ad0b39399b0489f9ced2a3bb37a6f0f90b1
Takeru Ohta <phjgt308@gmail.com>
2025-03-22 10:30:16 +0900

    Add `Other` error variant and implement `From<T>` trait for `Error` enum.

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,15 @@ use std::{borrow::Cow, io::IsTerminal};
 
 use crate::args::{Args, Metadata};
 
+pub type Result<T> = std::result::Result<T, Error>;
+
 pub enum Error {
     UnexpectedArg { metadata: Metadata, arg: String },
+    Other(Box<dyn std::fmt::Display>),
 }
 
 impl Error {
-    pub fn check_unexpected_arg(args: &Args) -> Result<(), Error> {
+    pub fn check_unexpected_arg(args: &Args) -> Result<()> {
         if let Some(unexpected_arg) = args.next_raw_arg_value() {
             Err(Error::UnexpectedArg {
                 metadata: args.metadata(),
@@ -17,29 +20,32 @@ impl Error {
             Ok(())
         }
     }
+
+    fn to_string(&self, is_terminal: bool) -> String {
+        let mut fmt = Formatter::new(is_terminal);
+        match self {
+            Error::UnexpectedArg { metadata, arg } => {
+                fmt.format_unexpected_arg(*metadata, arg);
+            }
+            Error::Other(e) => {
+                fmt.text = e.to_string();
+            }
+        }
+        fmt.text
+    }
+}
+
+impl<T: 'static + std::fmt::Display> From<T> for Error {
+    fn from(error: T) -> Self {
+        Self::Other(Box::new(error))
+    }
 }
 
 impl std::fmt::Debug for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut fmt = Formatter::new(std::io::stderr().is_terminal());
-        match self {
-            Error::UnexpectedArg { metadata, arg } => fmt.format_unexpected_arg(*metadata, arg),
-        }
-        write!(f, "{}", fmt.text)
+        write!(f, "{}", self.to_string(std::io::stderr().is_terminal()))
     }
 }
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut fmt = Formatter::new(false);
-        match self {
-            Error::UnexpectedArg { metadata, arg } => fmt.format_unexpected_arg(*metadata, arg),
-        }
-        write!(f, "{}", fmt.text)
-    }
-}
-
-impl std::error::Error for Error {}
 
 #[derive(Debug, Default)]
 struct Formatter {
@@ -92,14 +98,14 @@ mod tests {
         // Error without `--help`.
         let args = Args::new(["noargs", "--foo"].iter().map(|a| a.to_string()));
         let e = Error::check_unexpected_arg(&args).expect_err("should error");
-        assert_eq!(e.to_string(), "unexpected argument '--foo' found");
+        assert_eq!(e.to_string(false), "unexpected argument '--foo' found");
 
         // Error with `--help`.
         let mut args = Args::new(["noargs", "--foo"].iter().map(|a| a.to_string()));
         args.metadata_mut().help_option_name = Some("help");
         let e = Error::check_unexpected_arg(&args).expect_err("should error");
         assert_eq!(
-            e.to_string(),
+            e.to_string(false),
             r#"unexpected argument '--foo' found
 Try '--help' for more information."#
         );
