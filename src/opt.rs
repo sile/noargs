469e15ff6d1f73ff83042002b09af78bbe81aedb
Takeru Ohta <phjgt308@gmail.com>
2025-03-22 19:26:04 +0900

    Implement OptSpec::take method to parse command-line options and find matching options

--- src/opt.rs
+++ src/opt.rs
@@ -31,7 +31,91 @@ impl OptSpec {
     pub fn take(mut self, args: &mut Args) -> Opt {
         self.metadata = args.metadata();
         args.log_mut().record_opt(self);
-        todo!()
+
+        let mut pending = None;
+        for (index, raw_arg) in args.range_mut(self.min_index, self.max_index) {
+            if let Some(mut pending) = pending.take() {
+                match &mut pending {
+                    Opt::Long { value, .. } | Opt::Short { value, .. } => {
+                        *value = raw_arg.value.take()
+                    }
+                    _ => unreachable!(),
+                }
+                return pending;
+            }
+
+            let Some(value) = &mut raw_arg.value else {
+                continue;
+            };
+            if !value.starts_with('-') {
+                continue;
+            }
+
+            if value.starts_with("--") {
+                // Long name option.
+                if !value[2..].starts_with(self.name) {
+                    continue;
+                }
+                match value[2 + self.name.len()..].chars().next() {
+                    None => {
+                        pending = Some(Opt::Long {
+                            spec: self,
+                            index,
+                            value: None,
+                        });
+                    }
+                    Some('=') => {
+                        let opt_value = value[2 + self.name.len() + 1..].to_owned();
+                        raw_arg.value = None;
+                        return Opt::Long {
+                            spec: self,
+                            index,
+                            value: Some(opt_value),
+                        };
+                    }
+                    Some(_) => {}
+                }
+                continue;
+            } else if value[1..].chars().next() != self.short {
+                continue;
+            }
+
+            // Short name option.
+            match value[1..].chars().nth(1) {
+                None => {
+                    pending = Some(Opt::Short {
+                        spec: self,
+                        index,
+                        value: None,
+                    });
+                }
+                Some('=') => {
+                    let opt_name_len = self.short.map(|c| c.len_utf8()).unwrap_or(0);
+                    let opt_value = value[1 + opt_name_len + 1..].to_owned();
+                    raw_arg.value = None;
+                    return Opt::Short {
+                        spec: self,
+                        index,
+                        value: Some(opt_value),
+                    };
+                }
+                Some(_) => {}
+            }
+        }
+
+        if let Some(value) = self
+            .env
+            .and_then(|name| std::env::var(name).ok())
+            .filter(|v| !v.is_empty())
+        {
+            Opt::Env { spec: self, value }
+        } else if self.default.is_some() {
+            Opt::Default { spec: self }
+        } else if self.example.is_some() {
+            Opt::Example { spec: self }
+        } else {
+            Opt::None { spec: self }
+        }
     }
 }
 
@@ -46,15 +130,16 @@ pub enum Opt {
     Long {
         spec: OptSpec,
         index: usize,
-        value: String,
+        value: Option<String>,
     },
     Short {
         spec: OptSpec,
         index: usize,
-        value: String,
+        value: Option<String>,
     },
     Env {
         spec: OptSpec,
+        value: String,
     },
     Default {
         spec: OptSpec,
@@ -62,5 +147,7 @@ pub enum Opt {
     Example {
         spec: OptSpec,
     },
-    None,
+    None {
+        spec: OptSpec,
+    },
 }
